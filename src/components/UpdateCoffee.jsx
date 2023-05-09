1a89dc21e36aa59bcb8287a226342f2bad8d85d3
MasudBhuiya <masudbhuiyan1415@gmail.com>
2023-05-09 21:18:50 +0600

    first commit

---
+++ src/components/UpdateCoffee.jsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+const UpdateCoffee = () => {
+    return (
+        <div>
+            <h2>Updata a Coffee</h2>
+        </div>
+    );
+};
+
+export default UpdateCoffee;
